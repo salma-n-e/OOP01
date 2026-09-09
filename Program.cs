8d6351944ca56e011590d4840732a34dc8b1ce83
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-09 14:50:22 +0300

    1 A & B

--- Program.cs
+++ Program.cs
@@ -4,7 +4,14 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            //1 A
+            //  is a struct (a value type) ,Modifying the copied variable will not affect the original variable.
+
+
+            // 1 B
+            //is a class (a reference type) Modifying the object through either variable will
+            //affect both, as they share the same underlying data.
+
         }
     }
 }
