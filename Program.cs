9fa237b0a603e94bf46d25a260a1f53a34294647
سلمى نجم الدين محمد حسن بيومى العوفى stu <425254647@sha.edu.eg>
2026-09-09 14:59:44 +0300

    2 A & B

--- Program.cs
+++ Program.cs
@@ -9,8 +9,16 @@
 
 
             // 1 B
-            //is a class (a reference type) Modifying the object through either variable will
-            //affect both, as they share the same underlying data.
+            // is a class (a reference type) Modifying the object through either variable will
+            // affect both, as they share the same underlying data.
+
+
+            // 2 A
+            // 1- Public Fields 2- Lack of Data Validation 3- No Read-Only Control 
+
+            // 2 b 
+            // Private fields hide internal implementation details
+            // Public properties provide controlled access through get and set accessors 
 
         }
     }
